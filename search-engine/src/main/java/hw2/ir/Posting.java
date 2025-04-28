5aca73edee3415b9ab8e689c50e0b0dfd7954d10
ahmedkaram47 <112835403+ahmedkaram47@users.noreply.github.com>
2025-04-28 20:01:33 +0300

    Update Posting.java

--- search-engine/src/main/java/hw2/ir/Posting.java
+++ search-engine/src/main/java/hw2/ir/Posting.java
@@ -1,31 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hw2.ir;
 
-/**
- *
- * @author ehab
- */
- 
-/**
- * The Posting class represents a node in an inverted index's posting list.
- * Each Posting object contains information about a document and its term frequency.
- * It also supports linking to the next Posting in the list.
- */
+
+ // Posting = one document and the frequency of a word inside it.
+
 public class Posting {
+    public int docId;
+    public int frequency;
 
-    public Posting next = null;
-    int docId;
-    int dtf = 1;
-
-    Posting(int id, int t) {
-        docId = id;
-        dtf=t;
-    }
-    
-    Posting(int id) {
-        docId = id;
+    public Posting(int docId, int frequency) {
+        this.docId = docId;
+        this.frequency = frequency;
     }
 }
